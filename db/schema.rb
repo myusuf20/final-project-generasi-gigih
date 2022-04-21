7f7a408f3fb35b9582a986441877bbd968989892
Mohammad Yusuf <ysbcc.rys@gmail.com>
2022-04-21 10:45:40 +0700

    create generate model Category name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_21_031649) do
+ActiveRecord::Schema.define(version: 2022_04_21_034153) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "menu_items", force: :cascade do |t|
     t.string "name"
